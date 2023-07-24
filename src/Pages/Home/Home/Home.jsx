08524829a707dc948cfbc3fd8883a76d61ff0aa6
faizulsami <faizul.sami2021@gmail.com>
2023-07-24 23:01:09 +0600

    Update

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Search from '../Search/Search';
 
 import Gallery from '../Gallery/Gallery';
+import ShowReview from '../../ShowReview/ShowReview';
 
 const Home = () => {
     return (
         <div>
             <Search></Search>
             <Gallery></Gallery>
+            <ShowReview></ShowReview>
         </div>
     );
 };
